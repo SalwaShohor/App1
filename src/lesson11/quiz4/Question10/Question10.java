0121ec7a8f04a8cce30b812df3b7cf78b09caa8f
SalwaShohor <82011470+SalwaShohor@users.noreply.github.com>
2024-02-21 11:04:19 +0800

    Update Question10.java

--- src/lesson11/quiz4/Question10/Question10.java
+++ src/lesson11/quiz4/Question10/Question10.java
@@ -11,18 +11,14 @@ public class Question10 {
     }
 
     private static void change2(int val) {
-        // It seems like this method is trying to modify the static variable i1 and i2,
-        // but these variables are not static in the current class. I'll make them static.
         i1 = --val; // Line n3 
         i2 = val--; // Line n4
     }
 
     public static void main(String[] args) {
-        // Create an instance of Test to call the non-static method change1
 
         change1(5); // Line n5
 
-        // Call the static method change2 directly on the class
         change2(5); // Line n6
 
         // Print the values of i1 and i2
